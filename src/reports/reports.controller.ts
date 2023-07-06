84dc43ad3f91bef4042dc07334b79f52d87ba13f
TomVerkon <tverkon@gmail.com>
2023-07-05 23:30:35 -0400

    finished querybuilder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Param, Post, UseGuards, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards, Patch, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -21,16 +23,29 @@ export class ReportsController {
 
   @Patch('/:id')
   @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   @Serialize(ReportDto)
   async approveReport(@Body() body: ApproveReportDto, @Param('id') id: string) {
     return this.reportsService.changeApproval(parseInt(id), body.approved);
   }
 
+  // @Get()
+  // @UseGuards(AuthGuard)
+  // @Serialize(ReportDto)
+  // async getReportsByUser(@CurrentUser() user: User) {
+  //   const reports = await this.reportsService.find(user);
+  //   return reports;
+  // }
+
   @Get()
+  async getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
+
+  @Post('/update/:id')
   @UseGuards(AuthGuard)
   @Serialize(ReportDto)
-  async getReportsByUser(@CurrentUser() user: User) {
-    const reports = await this.reportsService.find(user);
-    return reports;
+  async updateReport(@Param('id') id: string, @Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.updateReport(parseInt(id), body, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
 import { User } from '../users/user.entity';
-import { ApproveReportDto } from './dtos/approve-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -27,5 +27,36 @@ export class ReportsService {
 
   async find(user: User) {
     return await this.repo.find({ where: { user: user } });
+  }
+
+  async createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select(['AVG(price)', 'price', `ABS(mileage - ${mileage}) as "mileage_diff"`])
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
+
+  async updateReport(id: number, dto: CreateReportDto, user: User) {
+    const report = await this.repo.findOneBy({ id });
+    if (!report) throw new NotFoundException('report not found');
+    report.make = dto.make;
+    report.model = dto.model;
+    report.year = dto.year;
+    report.lat = dto.lat;
+    report.lng = dto.lng;
+    report.mileage = dto.mileage;
+    report.price = dto.price;
+    if (!report.user) {
+      report.user = user;
+    }
+    return await this.repo.save(report);
   }
 }
